b7b6af8bb754da213cfbe5136f90f9a6a6f81df2
fleal-lamce <fleal@lamce.coppe.ufrj.br>
2025-08-07 16:18:41 -0300

    .

---
+++ estação_low_cost/sensor_temperatura/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+#define LM35_PIN 36  // GPIO36 = ADC1_CH0
+
+void setup() {
+  Serial.begin(115200);
+  analogReadResolution(12); // 12 bits → 0 a 4095
+  pinMode(LM35_PIN, INPUT);
+  Serial.println("Leitura do sensor LM35 iniciada...");
+}
+
+void loop() {
+  int adc = analogRead(LM35_PIN);  // valor cru do ADC
+  float tensao = adc * (3.3 / 4095.0);  // conversão para Volts (assumindo Vref = 3.3V)
+  float temperaturaC = tensao / 0.01;  // LM35: 10mV por grau → 0.01V por °C
+
+  // Impressão
+  Serial.print("ADC: ");
+  Serial.print(adc);
+  Serial.print(" | Tensão: ");
+  Serial.print(tensao, 3);
+  Serial.print(" V | Temperatura: ");
+  Serial.print(temperaturaC, 1);
+  Serial.println(" °C");
+
+  delay(1000);
+}
